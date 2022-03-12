4d5ad5e311a1885aa345430d110a73e21ffc864e
rchan3 <r.achiechan@gmail.com>
2022-03-11 23:34:56 -0500

    moved news div to a component

---
+++ src/components/home-news/Home-news.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const HomeNews = (props) => {
+  return (
+    <div className="news">
+      <h3>news</h3>
+      <div className="news-main"></div>
+    </div>
+  );
+};
+
+export default HomeNews;
