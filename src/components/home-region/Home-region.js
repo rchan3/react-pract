edc175f2bfaf65bd850dbefb3d5ce0de6ee51b86
rchan3 <r.achiechan@gmail.com>
2022-03-13 06:52:19 -0400

    make background image scale up and have white border on hover

--- src/components/home-region/Home-region.js
+++ src/components/home-region/Home-region.js
@@ -4,15 +4,22 @@ import "./styles.css";
 const HomeRegionComponent = (props) => {
   return (
     <div className="regions">
-      <div className="region-mondstadt">
-        <p>mondstadt</p>
+      <div className="region-wrapper">
+        <div className="region-mondstadt">
+          <p>mondstadt</p>
+        </div>
       </div>
-      <div className="region-liyue">
-        <p>liyue</p>
+      <div className="region-wrapper">
+        <div className="region-liyue">
+          <p>liyue</p>
+        </div>
       </div>
-      <div className="region-inazuma">
-        <p>inazuma</p>
+      <div className="region-wrapper">
+        <div className="region-inazuma">
+          <p>inazuma</p>
+        </div>
       </div>
+
       <div className="region-comingsoon">
         <p>coming soon</p>
       </div>
