c58e58a3bcdd3b35e2f13916f046e86adfd997a6
rchan3 <r.achiechan@gmail.com>
2022-03-13 05:40:02 -0400

    adjusted region div size and center region text name

--- src/components/home-region/Home-region.js
+++ src/components/home-region/Home-region.js
@@ -4,10 +4,18 @@ import "./styles.css";
 const HomeRegionComponent = (props) => {
   return (
     <div className="regions">
-      <div className="region-mondstadt">mondstadt</div>
-      <div className="region-liyue">liyue</div>
-      <div className="region-inazuma">inazuma</div>
-      <div className="region-comingsoon">coming soon</div>
+      <div className="region-mondstadt">
+        <p>mondstadt</p>
+      </div>
+      <div className="region-liyue">
+        <p>liyue</p>
+      </div>
+      <div className="region-inazuma">
+        <p>inazuma</p>
+      </div>
+      <div className="region-comingsoon">
+        <p>coming soon</p>
+      </div>
     </div>
   );
 };
