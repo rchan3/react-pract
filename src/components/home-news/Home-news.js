49e558bfac77240d6bdbd27973f8302edb67b916
rchan3 <r.achiechan@gmail.com>
2022-03-17 15:17:39 -0400

    get fetch working and remove comment sections

--- src/components/home-news/Home-news.js
+++ src/components/home-news/Home-news.js
@@ -14,37 +14,6 @@ const pagination = {
   },
 };
 
-// const newsImagePath = [
-//   {
-//     srcURL: require("./32fa76e072b091a28d3aa8577453cdfd_7473354622486228234.jpg"),
-//     srcAlt: "",
-//   },
-//   {
-//     srcURL: require("./235af4e62159910421fbcffbf1cd9c2e_2611120445102707012.jpg"),
-//     srcAlt: "",
-//   },
-//   {
-//     srcURL: require("./2022021610120592184.png"),
-//     srcAlt: "",
-//   },
-//   {
-//     srcURL: require("./2022021510344490430.jpg"),
-//     srcAlt: "",
-//   },
-// ];
-
-// const newsCarousel = () => {
-//   const carousel = [];
-//   newsImagePath.forEach((element) => {
-//     carousel.push(
-//       <SwiperSlide key={element.srcURL}>
-//         <img src={element.srcURL} alt={element.srcAlt} />
-//       </SwiperSlide>
-//     );
-//   });
-//   return carousel;
-// };
-// const HomeNews = (props) => {
 class HomeNews extends Component {
   constructor(props) {
     super(props);
@@ -63,23 +32,16 @@ class HomeNews extends Component {
     const carousel = [];
     if (this.state.newsImagePath.length > 0) {
       this.props.newsImagePath.forEach((element) => {
-        const srcURL = element.srcURL.replace(/'/g, "");
+        console.log(element.srcURL);
+
         carousel.push(
           <SwiperSlide key={element.srcURL}>
-            <img src={srcURL} alt={element.srcAlt} />
+            <img src={require(`${element.srcURL}`)} alt={element.srcAlt} />
           </SwiperSlide>
         );
-        console.log(element.srcURL.replace(/'/g, ""));
       });
     }
-    // this.props.newsImagePath.forEach((element) => {
-    //   carousel.push(
-    //     <SwiperSlide key={element.srcURL}>
-    //       <img src={element.srcURL} alt={element.srcAlt} />
-    //     </SwiperSlide>
-    //   );
-    //   console.log(element);
-    // });
+
     return carousel;
   };
 
