ace338b881982fde5a6033c8ec2708e3796c2276
rchan3 <r.achiechan@gmail.com>
2022-03-20 17:23:03 -0400

    added regionstar image to region divs + char images on hover + changed font family

--- src/components/home-region/Home-region.js
+++ src/components/home-region/Home-region.js
@@ -5,19 +5,46 @@ const HomeRegionComponent = (props) => {
   return (
     <div className="regions">
       <div className="region-wrapper">
-        <div className="region-mondstadt">
-          <p>mondstadt</p>
-        </div>
+        <img
+          className="region-star"
+          src={require("./assets/regionstar.png")}
+          alt=""
+        />
+        <p>mondstadt</p>
+        <img
+          className="region-hoverchar"
+          src={require("./assets/2021061518151058323.png")}
+          alt=""
+        />
+        <div className="region-mondstadt"></div>
       </div>
       <div className="region-wrapper">
-        <div className="region-liyue">
-          <p>liyue</p>
-        </div>
+        <img
+          className="region-star"
+          src={require("./assets/regionstar.png")}
+          alt=""
+        />
+        <p>liyue</p>
+        <img
+          className="region-hoverchar"
+          src={require("./assets/2021062410083822749.png")}
+          alt=""
+        />
+        <div className="region-liyue"></div>
       </div>
       <div className="region-wrapper">
-        <div className="region-inazuma">
-          <p>inazuma</p>
-        </div>
+        <img
+          className="region-star"
+          src={require("./assets/regionstar.png")}
+          alt=""
+        />
+        <p>inazuma</p>
+        <img
+          className="region-hoverchar"
+          src={require("./assets/2021071515201245150.png")}
+          alt=""
+        />
+        <div className="region-inazuma"></div>
       </div>
 
       <div className="region-comingsoon">
